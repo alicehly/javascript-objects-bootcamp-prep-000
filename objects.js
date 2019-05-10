e916314f3c3a02cd7bb3ba6729f0a4826d323b6b
Alice Ly Ms <alicehly@gmail.com>
2019-05-10 18:15:50 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName]=songTitle;
 }
 
-function removeFromPlayist(playlist, artistName, songTitle){
+function removeFromPlayist(playlist, artistName){
   delete playlist[artistName]
   return playlist
 }
