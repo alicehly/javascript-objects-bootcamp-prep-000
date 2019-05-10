45b6a57c04371504145aa4926062aab6b7366aa9
Alice Ly Ms <alicehly@gmail.com>
2019-05-10 18:14:09 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,6 @@
+var playlist = {vampireweekend: "horchata", haim: "days are gone", lizzo: "juice"};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName]=songTitle;
+}
+
